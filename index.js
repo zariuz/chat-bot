43ede5a9e8731d841e4e0c6a264a99955ffb6a2d
Evgeniy <evgeniy.nechetov@gmail.com>
2022-08-04 21:52:17 +0300

    edit seed

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import 'dotenv/config';
 import { Telegraf } from 'telegraf';
+import { PrismaClient } from '@prisma/client';
 
 const token = process.env.TOKEN;
 
@@ -7,16 +8,24 @@ if (!token) {
   throw new Error('Не задан token');
 }
 
+const prisma = new PrismaClient();
 const bot = new Telegraf(token);
 
+// prisma
+class App {
+  async init() {
+    await prisma.$connect();
+  }
+}
+
+const app = new App();
+app.init();
+
+// bot
 bot.start((ctx) => ctx.reply('Привет!'));
 
 bot.command('user', (ctx) => {
   ctx.reply(`name: ${ctx.from.username} id: ${ctx.from.id}`);
-});
-
-bot.on('text', (ctx) => {
-  ctx.reply(ctx.message.text);
 });
 
 bot.launch();
--- prisma/seed.js
+++ prisma/seed.js
@@ -1,11 +1,44 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
+
+const course = {
+  title: 'React за 5 минут!',
+  description: 'Освой курс по React за пять минут и стань Сеньор разработчиком!',
+  dateStart: '2022-08-15T19:20:30.451Z',
+  dateEnd: '2022-08-25T19:20:30.451Z',
+};
+
+const cities = [{ name: 'Москва' }, { name: 'Санкт-Петербург' }, { name: 'Минск' }];
+
+const specialties = [
+  {
+    title: 'DevOps',
+    description:
+      'методология автоматизации технологических процессов сборки, настройки и развёртывания программного обеспечения',
+  },
+  {
+    title: 'Frontend',
+    description:
+      'клиентская сторона пользовательского интерфейса к программно-аппаратной части сервиса.',
+  },
+  {
+    title: 'Backend',
+    description:
+      'программно-аппаратная часть сервиса, отвечающая за функционирование его внутренней части.',
+  },
+];
 
 const main = async () => {
   await prisma.$connect();
 
-  
+  await prisma.city.createMany({
+    data: cities.map((c) => c),
+  });
+  await prisma.speciality.createMany({
+    data: specialties.map((c) => c),
+  });
+  await prisma.course.create({ data: course });
 
   await prisma.$disconnect();
 };
